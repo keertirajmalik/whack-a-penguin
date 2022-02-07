bb7edfc81333001c05b76924684eaacbb24335ee
keertirajmalik <keertirajmalik@gmail.com>
2022-02-07 21:18:22 +0530

    added the game over SFX

--- whack a mole/GameScene.swift
+++ whack a mole/GameScene.swift
@@ -93,6 +93,7 @@ class GameScene: SKScene {
             gameOver.position = CGPoint(x: 512, y: 384)
             gameOver.zPosition = 1
             addChild(gameOver)
+            run(SKAction.playSoundFileNamed("sfx-defeat6.mp3", waitForCompletion: false))
             return
         }
         
